256d9707dbf6a992d0e96177217c391e7f0f9ede
Rohit Mahajan <tfrohit02@gmail.com>
2025-01-10 22:00:37 +0530

    Initial commit - web music player

---
+++ music.js
@@ -0,0 +1,38 @@
+let trackList = [
+  {
+    name: "2024",
+    path: "./music/2024.mp3",
+    img: "./images/img1.jpg",
+    singer: "Playboi Carti",
+  },
+  {
+    name: "Double Fantasy",
+    path: "./music/Double Fantasy.mp3",
+    img: "./images/img2.jpg",
+    singer: "The Weeknd",
+  },
+  {
+    name: "São Paulo",
+    path: "./music/São Paulo.mp3",
+    img: "./images/img3.jpg",
+    singer: "The Weeknd",
+  },
+  {
+    name: "Timeless",
+    path: "./music/Timeless.mp3",
+    img: "./images/img4.jpg",
+    singer: "The Weeknd",
+  },
+  {
+    name: "HIGHEST IN THE ROOM",
+    path: "./music/HIGHEST IN THE ROOM.mp3",
+    img: "./images/img10.jpg",
+    singer: "Travis Scott",
+  },
+  {
+    name: "tv off",
+    path: "./music/tv off.mp3",
+    img: "./images/img8.jpg",
+    singer: "Kendrick Lamar",
+  },
+];
